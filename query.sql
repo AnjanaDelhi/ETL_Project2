d07bb0f4e0cb13c55fbb946d1337c7f4b31a517e
AnjanaDelhi <46753092+AnjanaDelhi@users.noreply.github.com>
2020-10-24 13:10:37 -0500

    Finished

--- query.sql
+++ query.sql
@@ -4,15 +4,15 @@ drop table popular_artists;
 -- Create a new table for songs
 CREATE TABLE popular_songs (
 	id serial primary key, 
-  Song_Nominee VARCHAR(100) NOT NULL,
-  Weeks_at1 INT,
-  Weeks INT
+  song_nominee VARCHAR(100) NOT NULL,
+  weeks_at1 INT,
+  weeks INT
 );
 
 -- Create a new table for popular artists
 CREATE TABLE popular_artists (
 	id serial primary key, 
-  Artist VARCHAR(100) NOT NULL,
-  Weeks_at1 INT,
-  Weeks INT
+  artist VARCHAR(100) NOT NULL,
+  weeks_at1 INT,
+  weeks INT
 );
