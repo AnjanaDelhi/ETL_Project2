95eb237c7c715e58fe47045d5878cc2d4c0d1ac6
AnjanaDelhi <46753092+AnjanaDelhi@users.noreply.github.com>
2020-10-24 12:06:06 -0500

    Changes to SQL code

--- query.sql
+++ query.sql
@@ -1,6 +1,9 @@
+drop table popular_songs;
+drop table popular_artists;
+
 -- Create a new table for songs
 CREATE TABLE popular_songs (
-	id INT primary key, 
+	id serial primary key, 
   Song_Nominee VARCHAR(100) NOT NULL,
   Weeks_at1 INT,
   Weeks INT
@@ -8,7 +11,7 @@ CREATE TABLE popular_songs (
 
 -- Create a new table for popular artists
 CREATE TABLE popular_artists (
-	id INT primary key, 
+	id serial primary key, 
   Artist VARCHAR(100) NOT NULL,
   Weeks_at1 INT,
   Weeks INT
